36ce8930824ee99592dc47377ef2f6e39dde5fad
Chanuka Nuhan <chanukanuhan@gmail.com>
2023-03-31 13:07:44 +0530

    expired items page done

--- view/expiredItems.php
+++ view/expiredItems.php
@@ -1,1 +1,32 @@
-<h2>Expire items page</h2>
+<div class="container-fluid mt-5 text-center">
+    <h2 class="mb-4">Items about to expire</h2>
+    <div class="col-12">
+        <div class="container-fluid overflow-auto">
+            <table class="table table-striped-columns table-hover">
+                <thead class="table-dark">
+                <tr>
+                    <th>Id</th>
+                    <th>Product Name</th>
+                    <th>Quantity</th>
+                    <th>Buying Date</th>
+                    <th>Expiry Date</th>
+                    <th>Supplier Name</th>
+                    <th>Price</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody id="itemTable">
+
+                </tbody>
+            </table>
+            <nav aria-label="Page navigation">
+                <ul class="pagination pagination-sm justify-content-center">
+                    <li class="page-item active"><a class="page-link" href="">1</a></li>
+                    <li class="page-item"><a class="page-link" href="">2</a></li>
+                    <li class="page-item"><a class="page-link" href="">3</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
+<scripts src="/assets/js/expiredItems.js"></script>
