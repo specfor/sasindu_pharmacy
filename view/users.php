ce7416692ac972363126aea171ed58883aaf1096
vihanga <vihanga2003nimsara@gmail.com>
2023-04-02 17:05:06 +0530

    user page pagination removed

--- view/users.php
+++ view/users.php
@@ -158,13 +158,6 @@
                     <tbody id="userTable">
                     </tbody>
                 </table>
-                <nav aria-label="Page navigation">
-                    <ul class="pagination pagination-sm justify-content-center">
-                        <li class="page-item active"><a class="page-link" href="">1</a></li>
-                        <li class="page-item"><a class="page-link" href="">2</a></li>
-                        <li class="page-item"><a class="page-link" href="">3</a></li>
-                    </ul>
-                </nav>
             </div>
         </div>
     </div>
